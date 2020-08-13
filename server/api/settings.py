1d8f149fa4f89e356d9219261bba5c38cb81a245
guilherme <guilherme@guilhermeweb.dev>
2020-08-12 23:57:42 -0300

    Ocultação da chave secreta

--- server/api/settings.py
+++ server/api/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9a-)%56ff&eh&et@y41js++x1o8%3_opo(s-nym7@%pzfgy0qz'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
