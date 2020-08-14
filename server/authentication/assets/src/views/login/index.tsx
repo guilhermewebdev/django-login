e0a55a8483df01773b9fc49afba2d73038e338a5
guilherme <guilherme@guilhermeweb.dev>
2020-08-13 23:54:44 -0300

    Implementação de estilo

--- server/authentication/assets/src/views/login/index.tsx
+++ server/authentication/assets/src/views/login/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import './index.scss';
 
 const div = document.getElementById('app');
 
-render(
-    <React.StrictMode>
-        <div>Olá mundo</div>
-    </React.StrictMode>,
-    div
-)
+Object.assign(document, {
+    main: async function () {
+        render(
+            <React.StrictMode>
+                <div className="container">Olá mundo</div>
+            </React.StrictMode>,
+            div
+        )
+    }
+})
