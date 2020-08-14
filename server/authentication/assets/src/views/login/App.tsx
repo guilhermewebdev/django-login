b31e8a1bbba98911197760ed366b9dfff7492120
guilherme <guilherme@guilhermeweb.dev>
2020-08-14 02:17:32 -0300

    Implementação do formulário de login

--- server/authentication/assets/src/views/login/App.tsx
+++ server/authentication/assets/src/views/login/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 export default (props: any) => {
-    const { title } = props;
+    const { title, csrfToken } = props;
     return (
         <div className="container h-100">
             <div className="row align-items-center h-100">
@@ -11,9 +11,29 @@ export default (props: any) => {
                             <h4 className="text-center col-12">{title}</h4>
                         </div>
                         <div className="row">
-                            <div className="col-12">
-                                <form className="container">
-
+                            <div className="col-6">
+                                <form method='POST' className="container" acceptCharset="utf-8">
+                                    <input type="hidden" name="csrfmiddlewaretoken" value={csrfToken}/>
+                                    <p className="row">
+                                        <input
+                                            className="col-12"
+                                            type="text"
+                                            autoFocus
+                                            placeholder="Username"
+                                            name="username"
+                                        />
+                                    </p>
+                                    <p className="row">
+                                        <input
+                                            className="col-12"
+                                            type="password"
+                                            placeholder="Password"
+                                            name="password"
+                                        />
+                                    </p>
+                                    <p className="row">
+                                        <button className="col-12" type="submit">Log in</button>
+                                    </p>
                                 </form>
                             </div>
                         </div>
