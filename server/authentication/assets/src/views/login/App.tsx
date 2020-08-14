eef6eac8d3e4967087194a83230c306777b1e227
guilherme <guilherme@guilhermeweb.dev>
2020-08-14 01:32:51 -0300

    Alinhamento central

--- server/authentication/assets/src/views/login/App.tsx
+++ server/authentication/assets/src/views/login/App.tsx
@@ -3,16 +3,18 @@ import * as React from 'react';
 export default (props: any) => {
     const { title } = props;
     return (
-        <div className="container-fluid h-100">
-            <div className='row'></div>
-            <div className="row h-100">
-                <div className="col-12 h-100">
-                    <div className="container d-flex flex-column">
-                        <div className="row d-flex align-self-center flex-row justify-content-center align-items-center">
-                            <div className="col-md-4">
-                                <div className="container-fluid">
-                                    <div className="row form-header">{title}</div>
-                                </div>
+        <div className="container h-100">
+            <div className="row align-items-center h-100">
+                <div className="col-6 mx-auto">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <h4 className="text-center col-12">{title}</h4>
+                        </div>
+                        <div className="row">
+                            <div className="col-12">
+                                <form className="container">
+
+                                </form>
                             </div>
                         </div>
                     </div>
