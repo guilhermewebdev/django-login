02317fd9d846ed39cc333ae548ec44ddc6bb67ef
guilherme <guilherme@guilhermeweb.dev>
2020-08-13 21:42:07 -0300

    Configuração do react

--- server/api/settings.py
+++ server/api/settings.py
@@ -125,6 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    f'{BASE_DIR}/authentication/assets/dist/',
+STATICFILES_DIRS = [
+    'authentication/assets/dist/',
 ]
