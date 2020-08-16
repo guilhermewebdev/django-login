cd18f52bced646b532e10ab547f42b30a7a63e93
guilherme <guilherme@guilhermeweb.dev>
2020-08-16 17:34:21 -0300

    Criação de curvas aleatórias

--- server/authentication/assets/src/views/login/background.tsx
+++ server/authentication/assets/src/views/login/background.tsx
@@ -7,6 +7,10 @@ export default () => {
     })
     const canvasRef = React.useRef<HTMLCanvasElement>(null);
     const [size, setSize] = React.useState(sizeFactory());
+    const [position, setPosition] = React.useState({
+        x: 0,
+        y: 0,
+    })
     const updateSize = () => {
         setSize(sizeFactory())
     }
@@ -14,22 +18,37 @@ export default () => {
     React.useEffect(() => {
         const canvas: HTMLCanvasElement | null = canvasRef.current;
         const ctx = canvas?.getContext('2d');
-        const grd = ctx?.createLinearGradient(0, 0, size.width, size.height);
+        const randomXY = () => ({
+            x: Math.floor(Math.random() * size.width),
+            y: Math.floor(Math.random() * size.height),
+        })
+        const curves = new Array(30)
+            .fill({})
+            .map(() => ({
+                start: randomXY(),
+                control: randomXY(),
+                end: randomXY(),
+                size: Math.floor(Math.random() * 30)
+            }))
+        // Fill with gradient
+
         const render = () => {
-                ctx?.clearRect(0, 0, size.width, size.height);
-                ctx?.beginPath();
-            if (canvasRef.current) {
-                grd?.addColorStop(0, "red");
-                grd?.addColorStop(0.5, "transparent");
-                grd?.addColorStop(1, "black");
-
-                // Fill with gradient
+            ctx?.clearRect(0, 0, size.width, size.height);
+            curves.forEach(({ start, control, end, size }, index) => {
+                const grd = ctx?.createLinearGradient(start.x, start.y, end.x, end.y);
+                grd?.addColorStop(0, 'green');
+                grd?.addColorStop(0.5, 'cyan');
+                grd?.addColorStop(1, 'green');
+                ctx?.beginPath()
+                ctx?.moveTo(start.x, start.y);
+                ctx?.quadraticCurveTo(control.x, control.y, end.x, end.y);
                 Object.assign(ctx, {
-                    fillStyle: grd
+                    fillStyle: grd,
+                    strokeStyle: grd,
+                    lineWidth: size,
                 })
-                ctx?.fillRect(0, 0, size.width, size.height);
-
-            }
+                ctx?.stroke();
+            })
             return requestAnimationFrame(render);
         }
         const requestId = render();
